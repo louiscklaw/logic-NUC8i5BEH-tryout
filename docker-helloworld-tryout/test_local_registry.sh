f35892b89ae53f43bf40a4dca97e1ae5788e4542
Louis Law <louiscklaw@users.noreply.github.com>
2023-07-25 13:41:52 +0800

    misc update,

--- docker-helloworld-tryout/test_local_registry.sh
+++ docker-helloworld-tryout/test_local_registry.sh
@@ -5,3 +5,11 @@ set -ex
 docker pull ubuntu
 docker image tag ubuntu 192.168.10.61:5000/ubuntu
 docker push 192.168.10.61:5000/ubuntu
+
+docker pull traefik/whoami
+docker image tag traefik/whoami 192.168.10.61:5000/traefik/whoami
+docker push 192.168.10.61:5000/traefik/whoami
+
+docker pull mysql
+docker image tag mysql 192.168.10.61:5000/mysql
+docker push 192.168.10.61:5000/mysql
